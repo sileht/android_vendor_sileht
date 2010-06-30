8e3f6fc7066059505034293fc3d58d51df2e2c16
Mehdi ABAAKOUK <theli48@gmail.com>
2010-06-30 22:26:54 +0200

    Some to match new Cyanogen Layout

--- products/sileht_sapphire.mk
+++ products/sileht_sapphire.mk
@@ -20,25 +20,30 @@ $(call inherit-product, device/htc/sapphire/full_sapphire.mk)
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cyanogen/products/common.mk)
 
-TARGET_KERNEL_DIR := kernel
+TARGET_KERNEL_DIR := kernel-msm
 TARGET_KERNEL_CONFIG := cyanogen_msm_defconfig
+
+
+CVERSION := $(shell sed -n '/[[:space:]]*ro.modversion=CyanogenMod-/s///gp' vendor/cyanogen/products/cyanogen_sapphire.mk | tail -1)-mod
+VERSION_INDEX := $(shell i=$$(ls -1 $(TARGET_ZIP)*-signed.zip 2>/dev/null | sed -n 's/$(TARGET_ZIP)\([[:digit:]]*\)-signed.zip/\1/gp' | sort -n | tail -1) ; echo $$((i+1)))
+
 
 PRODUCT_NAME := sileht_sapphire
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := sapphire
 PRODUCT_MODEL := HTC Magic (Sileht)
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRF83 BUILD_DISPLAY_ID=FRF83 PRODUCT_NAME=passion BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2/FRF83/42295:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRF83 BUILD_DISPLAY_ID=FRF83 PRODUCT_NAME=passion BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2/FRF83/42295:user/release-keys TARGET_BUILD_TYPE=userdebug
 PRIVATE_BUILD_DESC="passion-user 2.2 FRF83 42295 release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.modversion=CyanogenMod-$(CVERSION)$(VERSION_INDEX) \
 	ro.ril.hep=1 \
 	ro.ril.enable.dtm=1 \
 	ro.ril.enable.a53=1 \
 	ro.ril.hsdpa.category=8 \
 	ro.ril.hsupa.category=5 \
 	ro.ril.enable.3g.prefix=1 \
-
 #	ro.ril.htcmaskw1.bitmask = 4294967295 \
 #	ro.ril.htcmaskw1 = 14449 \
 
@@ -46,24 +51,18 @@ PRODUCT_COPY_FILES += \
     vendor/sileht/prebuilt/common/etc/bashrc:system/etc/bashrc \
 	vendor/cyanogen/prebuilt/sapphire/media/bootanimation.zip:system/media/bootanimation.zip \
 
-#CVERSION := $(shell sed -n '/[[:space:]]*ro.modversion=CyanogenMod-/s///gp' vendor/cyanogen/products/cyanogen_dream_sapphire.mk | tail -1)-mod
-CVERSION := 6.0.0-SA-test0
+USE_CAMERA_STUB := false
+
+
+
 
 TARGET_ZIP := update-sm-$(CVERSION)
-
-VERSION_INDEX := $(shell i=$$(ls -1 $(TARGET_ZIP)*-signed.zip 2>/dev/null | sed -n 's/$(TARGET_ZIP)\([[:digit:]]*\)-signed.zip/\1/gp' | sort -n | tail -1) ; echo $$((i+1)))
-
-
-PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-$(CVERSION)$(VERSION_INDEX)
-
 FINAL_TARGET_ZIP := $(TARGET_ZIP)$(VERSION_INDEX)-signed.zip
 
 $(FINAL_TARGET_ZIP): bacon
 	@echo "Finish $(FINAL_TARGET_ZIP)"
 	./vendor/cyanogen/tools/squisher
-	#mv $$OUT/update-cm-$(CVERSION)$(VERSION_INDEX)-signed.zip $(FINAL_TARGET_ZIP)
-
+	cp $$OUT/update-cm-$(CVERSION)$(VERSION_INDEX)-signed.zip $(FINAL_TARGET_ZIP)
 
 it: $(FINAL_TARGET_ZIP)
 
