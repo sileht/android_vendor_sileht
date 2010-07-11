516700c3135b885036e484779dc4a707d60a2946
Abaakouk Mehdi <theli48@gmail.com>
2010-07-11 10:38:34 +0200

    Cyanogen sync

--- products/sileht_dream_sapphire.mk
+++ products/sileht_dream_sapphire.mk
@@ -10,12 +10,7 @@ $(call inherit-product, vendor/cyanogen/products/common.mk)
 # Include GSM-only stuff
 $(call inherit-product, vendor/cyanogen/products/gsm.mk)
 
-PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/cyanogen/prelink-linux-arm-ds.map
 
-# Build kernel
-PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=
-PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_DIR=kernel-msm
-PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_CONFIG=cyanogen_msm_defconfig
 
 CVERSION := $(shell sed -n '/[[:space:]]*ro.modversion=CyanogenMod-/s///gp' vendor/cyanogen/products/cyanogen_dream_sapphire.mk| tail -1)-mod
 TARGET_ZIP := update-sm-$(CVERSION)
@@ -26,8 +21,23 @@ PRODUCT_BRAND := google
 PRODUCT_DEVICE := dream_sapphire
 PRODUCT_MODEL := HTC Magic (Sileht)
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRF91 BUILD_DISPLAY_ID=FRF91 PRODUCT_NAME=sapphire BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2/FRF91/43546:user/release-keys TARGET_BUILD_TYPE=userdebug BUILD_VERSION_TAGS=release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRF91 BUILD_DISPLAY_ID=FRF91 BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2/FRF91/43546:user/release-keys
 PRIVATE_BUILD_DESC="sapphire-user 2.2 FRF91 43546 release-keys"
+
+PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/cyanogen/prelink-linux-arm-ds.map
+
+# Build kernel
+PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=
+PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_DIR=kernel-msm
+PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_CONFIG=cyanogen_msm_defconfig
+
+# Extra DS overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/dream_sapphire
+
+# This file is used to install the correct audio profile when booted
+PRODUCT_COPY_FILES += \
+    vendor/cyanogen/prebuilt/dream_sapphire/etc/init.d/02audio_profile:system/etc/init.d/02audio_profile
+
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.modversion=CyanogenMod-$(CVERSION)$(VERSION_INDEX) \
@@ -40,9 +50,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
 #	ro.ril.htcmaskw1.bitmask = 4294967295 \
 #	ro.ril.htcmaskw1 = 14449 \
 
+# Use the audio profile hack
+WITH_DS_HTCACOUSTIC_HACK := true
+
+# Use Windows Media
+WITH_WINDOWS_MEDIA := true
+
+
 PRODUCT_COPY_FILES += \
     vendor/sileht/prebuilt/common/etc/bashrc:system/etc/bashrc \
-	vendor/cyanogen/prebuilt/dream_sapphire/media/bootanimation.zip:system/media/bootanimation.zip
+	vendor/cyanogen/prebuilt/dream_sapphire/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/cyanogen/prebuilt/dream_sapphire/etc/init.d/02audio_profile:system/etc/init.d/02audio_profile \
+    vendor/cyanogen/prebuilt/dream_sapphire/etc/AudioPara_dream.csv:system/etc/AudioPara_dream.csv \
+    vendor/cyanogen/prebuilt/dream_sapphire/etc/AudioPara_sapphire.csv:system/etc/AudioPara_sapphire.csv
 
 FINAL_TARGET_ZIP := $(TARGET_ZIP)$(VERSION_INDEX)-signed.zip
 $(FINAL_TARGET_ZIP): bacon
