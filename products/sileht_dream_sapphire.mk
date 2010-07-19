86ff24cc531c8bbdc192e0af63d87ee60ab4a32d
Abaakouk Mehdi <theli48@gmail.com>
2010-07-19 11:06:11 +0200

    Sync cyanogen

--- products/sileht_dream_sapphire.mk
+++ products/sileht_dream_sapphire.mk
@@ -7,10 +7,6 @@ $(call inherit-product, vendor/cyanogen/products/common.mk)
 # Include GSM-only stuff
 $(call inherit-product, vendor/cyanogen/products/gsm.mk)
 
-# Use eu GPS settings
-$(call inherit-product, device/common/gps/gps_eu_supl.mk)
-
-
 CVERSION := $(shell sed -n '/[[:space:]]*ro.modversion=CyanogenMod-/s///gp' vendor/cyanogen/products/cyanogen_dream_sapphire.mk| tail -1)-mod
 TARGET_ZIP := update-sm-$(CVERSION)
 VERSION_INDEX := $(shell i=$$(ls -1 $(TARGET_ZIP)*-signed.zip 2>/dev/null | sed -n 's/$(TARGET_ZIP)\([[:digit:]]*\)-signed.zip/\1/gp' | sort -n | tail -1) ; echo $$((i+1)))
@@ -21,7 +17,7 @@ VERSION_INDEX := $(shell i=$$(ls -1 $(TARGET_ZIP)*-signed.zip 2>/dev/null | sed 
 PRODUCT_NAME := sileht_dream_sapphire
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := dream_sapphire
-PRODUCT_MODEL := HTC Magic (Sileht)
+PRODUCT_MODEL := Dream/Sapphire
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRF91 BUILD_DISPLAY_ID=FRF91 BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2/FRF91/43546:user/release-keys
 PRIVATE_BUILD_DESC="sapphire-user 2.2 FRF91 43546 release-keys"
@@ -42,7 +38,6 @@ PRODUCT_COPY_FILES += \
 
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.modversion=CyanogenMod-$(CVERSION)$(VERSION_INDEX) \
 	ro.ril.hep=1 \
 	ro.ril.enable.dtm=1 \
 	ro.ril.enable.a53=1 \
@@ -51,6 +46,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.ril.enable.3g.prefix=1
 #	ro.ril.htcmaskw1.bitmask = 4294967295 \
 #	ro.ril.htcmaskw1 = 14449 \
+
+#
+# Set ro.modversion
+#
+ifdef CYANOGEN_NIGHTLY
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=CyanogenMod-6-$(shell date +%m%d%Y)-NIGHTLY-DS
+else
+    PRODUCT_PROPERTY_OVERRIDES += \
+    	ro.modversion=CyanogenMod-$(CVERSION)$(VERSION_INDEX)
+endif
+
 
 # Use the audio profile hack
 WITH_DS_HTCACOUSTIC_HACK := true
@@ -67,14 +74,11 @@ PRODUCT_COPY_FILES +=  \
     vendor/cyanogen/prebuilt/dream_sapphire/etc/AudioPara_dream.csv:system/etc/AudioPara_dream.csv \
     vendor/cyanogen/prebuilt/dream_sapphire/etc/AudioPara_sapphire.csv:system/etc/AudioPara_sapphire.csv
 
-PRODUCT_COPY_FILES += \
-    vendor/sileht/prebuilt/common/etc/bashrc:system/etc/bashrc 
-
 FINAL_TARGET_ZIP := $(TARGET_ZIP)$(VERSION_INDEX)-signed.zip
 $(FINAL_TARGET_ZIP): bacon
 	@echo "Finish $(FINAL_TARGET_ZIP)"
 	./vendor/cyanogen/tools/squisher
-	cp $$OUT/update-cm-$(CVERSION)$(VERSION_INDEX)-signed.zip $(FINAL_TARGET_ZIP)
+	cp $${OUT}/update-cm-$(CVERSION)$(VERSION_INDEX)-signed.zip $(FINAL_TARGET_ZIP)
 
 it: $(FINAL_TARGET_ZIP)
 
